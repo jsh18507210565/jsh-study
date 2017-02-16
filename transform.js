2dd8702ae9cb40d7b37cabc228796510581820ac
jsh18507210565 <18507210565@wo.cn>
2017-02-16 11:39:34 +0800

    Add files via upload

---
+++ transform.js
@@ -0,0 +1,13 @@
+﻿
+var ele ;
+$.fn.extend({
+    rotate: function () {
+        ele = this ;
+        setInterval('singleTransform()',5);
+    }
+});
+var degree = 0;
+function singleTransform() {
+    degree = degree + 50 * Math.PI / 180;
+    ele.css("transform","rotateY("+degree+"deg)");
+}
